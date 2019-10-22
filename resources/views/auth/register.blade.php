7f0ee1846d61dfb39fdcd1f4ac11bb0504b6feac
Thanapass <thanapass.p@ku.th>
2019-10-22 12:19:56 +0700

    register01

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,6 +32,34 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="firstname" class="col-md-4 col-form-label text-md-right">{{ __('Firstname') }}</label>
+
+                            <div class="col-md-6">
+                                <input style="width:500px;height:50px;font-size:20px" id="firstname" type="text" class="form-control @error('firstname') is-invalid @enderror" name="firstname" value="{{ old('firstname') }}" required autocomplete="firstname" autofocus>
+
+                                @error('firstname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Lastname') }}</label>
+
+                            <div class="col-md-6">
+                                <input style="width:500px;height:50px;font-size:20px" id="lastname" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname" autofocus>
+
+                                @error('lastname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+                        <div class="form-group row">
                             <label  for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -25,7 +25,7 @@
   <div class="divgallery"> 
   <p class="usernamepost"><i class="fas fa-user"></i> <a href="{{route('users.show',['user'=>$post->user->id])}}">{{$post->user->name}}</a></p>
     <!-- <p>{{$post->detail}}</p> -->
-    <a href="{{route('posts.show' , ['post' => $post->id]) }}"><img src="/image/{{$post->picture}}" width="500px"></a>
+    <a href="{{route('posts.show' , ['post' => $post->id]) }}"><img src="/image/{{$post->picture}}" width="100%"></a>
     <div class="like">
       @auth
       <button type="button" class="btn btn-light"><i class="fab fa-gratipay pink"></i> Like</button>
